0552bc79870e7a913000c468ede065b599dd87c6
Peter Oas <oas.peter@gmail.com>
2018-10-30 14:56:30 -0400

    defined variables and printed to the console

---
+++ house-hunting.py
@@ -0,0 +1,6 @@
+annual_salary = float(input('Enter your annual salary: '))
+percent_of_salary = float(input('Enter the percent of your salary to save, as a decimal: '))
+total_cost = float(input('Enter the cost of your dream home: '))
+print (annual_salary)
+print (percent_of_salary)
+print (total_cost)
