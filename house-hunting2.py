3236fab9fa7682034a88c5c23f3ae4d5eeaba24f
Peter Oas <oas.peter@gmail.com>
2018-10-30 21:24:34 -0400

    1st attempt on hard mode thru house-hunting2

--- house-hunting2.py
+++ house-hunting2.py
@@ -6,13 +6,18 @@
 # Assume you are going to dedicate a certain amount of your salary each month to saving for the down payment. Call that portion_saved​. This variable should be in decimal form (i.e. 0.1 for 10%).
 # At the end of each month, your savings will be increased by the return on your investment, plus a percentage of your monthly salary ​(annual salary / 12).
 
+
 annual_salary = float(input('Enter your annual salary: '))
 percent_of_salary = float(input('Enter the percent of your salary to save, as a decimal: '))
+while r = False:
+    r = float(input('Enter the expected rate of return: '))
+    
 total_cost = float(input('Enter the cost of your dream home: '))
 
 current_savings = 0
 months_to_down = 0
 r = .04
+
 
 while current_savings < (total_cost * .25):
     monthly_savings = (annual_salary * percent_of_salary / 12) + (current_savings * r / 12)
